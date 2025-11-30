a32542d54e0f68ac7dca9d9a6fbdd26ded95e693
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-11-30 03:09:28 +0100

    PPfMA adding lab 1
    Calculates average with and without shared memory

---
+++ Parallel Programming for Multi-Core Architectures/Labs/Lab1/CalculateAverageShared.cu
@@ -0,0 +1,106 @@
+/*
+Implement solution which calculates the average value over integer numbers within
+array of length N. Numbers should be randomly generated, before calculations,
+within defined range <A;B>.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+__host__ void errorexit(const char *s) 
+{
+    printf("\n%s",s);	
+    exit(EXIT_FAILURE);	 	
+}
+
+void generateRandomNumbers(int *arr, int N, int A, int B) 
+{
+	srand(42);
+    for (int i = 0; i < N; i++) { arr[i] = A + rand() % (B - A +1);}
+}
+
+__global__ void calculateAvg(int *data, double *avg, int N) 
+{
+    extern __shared__ double shared[]; 
+    
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int tid = threadIdx.x;
+    
+    shared[tid] = (idx < N) ? (double)data[idx]/N : 0.0;
+    __syncthreads();
+
+    // Redukcja równoległa (trochę jak merge sort)
+    for(int i = 1; i < blockDim.x; i *= 2)
+    {
+        if(tid % (2 * i) == 0)
+        {
+            shared[tid] += shared[tid + i];
+        }
+        __syncthreads();
+    }
+    
+    if (tid == 0) {
+        atomicAdd(avg, shared[0]);
+    }
+}
+
+int main(int argc,char **argv) 
+{
+
+    int threadsinblock=1024;
+    int blocksingrid;
+
+    int N = argc > 1 ? atoi(argv[1]) : 100;
+    int A=0;
+    int B=100;
+    
+ 	cudaEvent_t start, stop;
+    float milliseconds = 0;
+
+	int *randomNumbers = (int *)malloc(N * sizeof(int));
+    if (randomNumbers == NULL) 
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+	generateRandomNumbers(randomNumbers, N,A,B);
+
+	blocksingrid = ceil((double)N/threadsinblock);
+
+	printf("The kernel will run with: %d blocks\n", blocksingrid);
+
+    int *randomNumbersDevice;
+    double *avgDevice;
+    double avgHost = 0.0;
+
+	cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
+
+	cudaMalloc((void **)&randomNumbersDevice, N * sizeof(int));
+    cudaMalloc((void **)&avgDevice, sizeof(double));
+
+    cudaMemcpy(randomNumbersDevice, randomNumbers, N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemset(avgDevice, 0, sizeof(double));
+
+    calculateAvg<<<blocksingrid, threadsinblock, threadsinblock * sizeof(double)>>>(randomNumbersDevice, avgDevice, N);
+
+    cudaMemcpy(&avgHost, avgDevice, sizeof(double), cudaMemcpyDeviceToHost);
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    //results
+    double average = avgHost;
+    printf("Average: %.5f\n", average);
+    printf("Kernel execution time: %.3f ms\n", milliseconds);
+
+    free(randomNumbers);
+    cudaFree(randomNumbersDevice);
+    cudaFree(avgDevice);
+
+    return 0;
+
+}
